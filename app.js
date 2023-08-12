b84bd340c523c6a23e4eebaae695f4d07032b69e
sunil-bind <sunil329021@gmail.com>
2023-08-12 22:46:42 +0530

    hot to put tags under another tag without using jsx

--- app.js
+++ app.js
@@ -1,9 +1,28 @@
+/**
+ *
+ * <div id="parent">
+ *  <div id="child">
+ *      <h1>i ma h1 tag</h1>
+ *      <h2>i am h2 tag</h2>
+ *  </div>
+ * </div>
+ */
+
 const heading = React.createElement(
-  "h1",
+  "div",
   {
-    id: "heading",
+    id: "parent",
   },
-  "hello world in react"
+  React.createElement(
+    "div",
+    {
+      id: "child",
+    },
+    [
+      React.createElement("h1", {}, "i am h1 tag"),
+      React.createElement("h2", {}, "i am h2 tag"),
+    ]
+  )
 );
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(heading);
